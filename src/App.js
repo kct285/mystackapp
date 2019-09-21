d137cb441f5e34f08f49967448f0335b94c2524f
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-09-21 10:26:46 +0800

    addedbutton

--- src/App.js
+++ src/App.js
@@ -5,20 +5,145 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class ="flex">
+       <div id="title">
+         <h1> The Stack</h1>
+         <h2> The Negative Impacts of a Digital Life</h2>
+       </div>
+
+       <div id="lab3">
+         <p><strong>03</strong></p>
+       </div>
+     </div>
+
+     <hr class="line"></hr>
+
+     <br></br>
+
+     <div class="images">
+       <div class="hovertext">
+         <img id="1" src="https://cdn1.imggmi.com/uploads/2019/9/13/929a675fe0d6ba036039254cfb6e5b38-full.png"/> 
+         <div class="overlay">
+         <div class="overlay-text"> <strong>User:</strong>
+         <br></br> The negative impact of a digital life that I chose to focus on is <br></br>
+                        how social media and the rise of photoshopped images distorts many <br></br>
+                        digital user’s perception of reality. Many young people often posts <br></br>
+                        perfect pictures on platforms such as Facebook, Twitter, <br></br>
+                        Youtube, and in this example, Instagram. The purpose of this is <br></br>
+                        usually just to share or advertise experiences and products on social <br></br>
+                        media. However, with this, also comes with the formation of a false <br></br>
+                        sense of reality in which users sometimes attempt to achieve —  <br></br>
+                        affecting their mental health. 
+                        </div>
+         </div>
+       </div>
+
+       <br></br>
+       <br></br>
+
+       <div class="hovertext">
+       <img id="2" src="https://cdn1.imggmi.com/uploads/2019/9/13/390f9b338dab6e2e50195cc809f16f5c-full.jpg" />
+       <div class="overlay">
+         <div class="overlay-text"> <strong>Interface:</strong>
+         <br></br> There are many interface features on Instagram that <br></br>
+                        encourages sharing and posting through its Instagram feed, stories, <br></br>
+                         TV and direct messaging. For example, the feed and discover page<br></br>
+                          are both designed as an endless scroll — allowing users to use <br></br>
+                          the app for hours on end. Instagram stories encourages users to <br></br>
+                          constantly keep checking the app for updates on their following’s <br></br>
+                          stories as they only appear for 24 hours and is regularly updated.</div>
+         </div>
+       </div>
+
+       <br></br>
+       <br></br>
+
+       <div class="hovertext">
+       <img id="3" src="https://cdn1.imggmi.com/uploads/2019/9/13/a3a3d389ccd4c0e597b105dcb046c405-full.png" />
+       <div class="overlay">
+         <div class="overlay-text"> <strong>Address:</strong>
+         <br></br> Wifi is needed to access Instagram. Hence, usually <br></br>
+                            people would be using their home wifi or their mobile data <br></br>
+                             via 4G or LTE.</div> 
+         </div>
+       </div>
+
+       <br></br>
+       <br></br>
+
+
+       <div class="hovertext">
+       <img id="4" src="https://cdn1.imggmi.com/uploads/2019/9/13/4c4b22d364b3407cafacd53bd1c0a3c0-full.png"/>
+       <div class="overlay"> 
+         <div class="overlay-text"> <strong>City:</strong>
+         <br></br> Currently living in Shanghai, NYU Shanghai students <br></br>
+                            would all be relying on some sort of Chinese internet <br></br>
+                            provider, perhaps China Telecom. Since we are in China, <br></br>
+                            VPN is required to access Instagram. </div>
+         </div>
+       </div>
+
+       <br></br>
+       <br></br>
+
+
+       <div class="hovertext">
+       <img id="5" src="https://cdn1.imggmi.com/uploads/2019/9/13/aade497a13fc8f441e69011ec8f5405e-full.jpg"/> 
+       <div class="overlay"> 
+         <div class="overlay-text"> <strong>Cloud:</strong>
+         <br></br> All Instagram users and profile information would<br></br>
+                            all be stored in the Instagram server. This huge server <br></br>
+                            allows the organisation and storage of user data and <br></br>
+                            interaction. </div>
+         </div>
+       </div>
+
+       <br></br>
+       <br></br>
+
+       <div class="hovertext">
+       <img id="6" src="https://cdn1.imggmi.com/uploads/2019/9/13/3c25674b33fdfbc706d3338c5fb83f9e-full.png"/>
+       <div class="overlay">  
+         <div class="overlay-text"> <strong>Earth:</strong>
+         <br></br> From anywhere around the world, anyone could post <br></br>
+                            whatever content they desire on Instagram. In this image,<br></br>
+                             a person is photoshopping an image to get it ready for <br></br>
+                             posting on Instagram in order for the image to look <br></br>
+                             “better”. Whilst that may be the case, doing this simply <br></br>
+                             creates a false image of perfection that may not be attainable<br></br>
+                              to its audience. Once again, pressures and possibly <br></br>
+                              mental health issues may arise from this act — a negative <br></br>
+                              impact of a digital life. </div>
+         </div>
+       </div>
+
+       <br></br>
+       <br></br>
+       <br></br>
+       <br></br>
+
+
+
+     </div>
+
+     <div class="buttonwrap">   
+     <a href="https://mitpress.mit.edu/books/stack">
+     <button id="learnmore"> Learn More About the Stack </button>
+
+     </a>
+
+ </div>
+
+
+
+
+     <br></br>
+       <br></br>
+       <br></br>
+       <br></br>
+
+
+
     </div>
   );
 }
